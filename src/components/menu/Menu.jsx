1de339e502d7da87912a3ac74005a6fb06a5eaf7
KarlaR <karlarobertati@gmail.com>
2024-01-03 19:32:32 -0300

    resposivo atualizado

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
             <li><a href="#menu">Menu</a></li>
             <li><a href="#sobre">Sobre</a></li>
             <li><a href="#servicos">Serviços</a></li>
-            {/* <li><a href="#depoimentos">Depoimentos</a></li> */}
+            <li><a href="#depoimentos">Depoimentos</a></li>
             <li><a href="#contatos">Contatos</a></li>
         </ul>
         
